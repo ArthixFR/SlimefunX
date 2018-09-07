f40f5a3cf5126329a7ddbfa27d27a486322f7625
Florian CUNY <poslovitch@gmail.com>
2018-09-07 22:47:26 +0200

    Added SlimefunMachine (extends SlimefunItem)

---
+++ src/main/java/fr/poslovitch/slimefunx/api/machines/SlimefunMachine.java
@@ -0,0 +1,14 @@
+package fr.poslovitch.slimefunx.api.machines;
+
+import fr.poslovitch.slimefunx.api.categories.Category;
+import fr.poslovitch.slimefunx.api.interfaces.Placeable;
+import fr.poslovitch.slimefunx.api.items.SlimefunItem;
+import org.bukkit.inventory.ItemStack;
+
+public class SlimefunMachine extends SlimefunItem implements Placeable {
+
+    public SlimefunMachine(String id, Category category, ItemStack item, int researchCost) {
+        super(id, category, item, researchCost);
+    }
+
+}
