adb997333357bb313ea0c3ede31e1ccf574b51b6
Florian CUNY <poslovitch@gmail.com>
2018-09-07 22:46:08 +0200

    Added Javadoc in Researchable

--- src/main/java/fr/poslovitch/slimefunx/api/interfaces/Researchable.java
+++ src/main/java/fr/poslovitch/slimefunx/api/interfaces/Researchable.java
@@ -1,7 +1,15 @@
 package fr.poslovitch.slimefunx.api.interfaces;
 
+/**
+ * Represents the ability for a {@link fr.poslovitch.slimefunx.api.base.SlimefunObject} to be researched by a {@link org.bukkit.entity.Player}.
+ *
+ * @author Poslovitch
+ */
 public interface Researchable {
 
+    /**
+     * Returns the XP levels cost of the research.
+     * @return the cost of the research in XP levels.
+     */
     int getResearchCost();
-
 }
