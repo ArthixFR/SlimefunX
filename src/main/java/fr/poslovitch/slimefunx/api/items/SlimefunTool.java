46ce566152857ee1ba726b531998aef54af2d4b3
Florian CUNY <poslovitch@gmail.com>
2018-09-08 16:01:35 +0200

    Made SlimefunTool and SlimefunMachine implement Interactable

--- src/main/java/fr/poslovitch/slimefunx/api/items/SlimefunTool.java
+++ src/main/java/fr/poslovitch/slimefunx/api/items/SlimefunTool.java
@@ -1,11 +1,22 @@
 package fr.poslovitch.slimefunx.api.items;
 
 import fr.poslovitch.slimefunx.api.categories.Category;
+import fr.poslovitch.slimefunx.api.interfaces.Interactable;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
-public class SlimefunTool extends SlimefunItem {
+public class SlimefunTool extends SlimefunItem implements Interactable {
 
-    public SlimefunTool(String id, Category category, ItemStack item, String[][] recipePattern, int researchCost) {
+    private final Listener interactionListener;
+
+    public SlimefunTool(String id, Category category, ItemStack item, String[][] recipePattern, int researchCost, Listener interactionListener) {
         super(id, category, item, recipePattern, researchCost);
+
+        this.interactionListener = interactionListener;
+    }
+
+    @Override
+    public Listener getInteractionListener() {
+        return interactionListener;
     }
 }
--- src/main/java/fr/poslovitch/slimefunx/api/machines/SlimefunMachine.java
+++ src/main/java/fr/poslovitch/slimefunx/api/machines/SlimefunMachine.java
@@ -1,13 +1,24 @@
 package fr.poslovitch.slimefunx.api.machines;
 
 import fr.poslovitch.slimefunx.api.categories.Category;
+import fr.poslovitch.slimefunx.api.interfaces.Interactable;
 import fr.poslovitch.slimefunx.api.interfaces.Placeable;
 import fr.poslovitch.slimefunx.api.items.SlimefunItem;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
-public class SlimefunMachine extends SlimefunItem implements Placeable {
+public class SlimefunMachine extends SlimefunItem implements Placeable, Interactable {
+
+    private final Listener interactionListener;
 
     public SlimefunMachine(String id, Category category, ItemStack item, String[][] recipePattern, int researchCost) {
         super(id, category, item, recipePattern, researchCost);
+
+        this.interactionListener = new Listener(){};
+    }
+
+    @Override
+    public Listener getInteractionListener() {
+        return interactionListener;
     }
 }
