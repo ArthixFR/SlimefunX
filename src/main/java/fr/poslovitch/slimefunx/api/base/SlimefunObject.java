a5a614f2519258c040122d22b2b0b4c05349fd2c
Florian CUNY <poslovitch@gmail.com>
2018-09-08 15:38:28 +0200

    Added "slimefun:hash" NBTTag to the item from SlimefunObject

--- src/main/java/fr/poslovitch/slimefunx/api/base/SlimefunObject.java
+++ src/main/java/fr/poslovitch/slimefunx/api/base/SlimefunObject.java
@@ -2,11 +2,15 @@ package fr.poslovitch.slimefunx.api.base;
 
 import fr.poslovitch.slimefunx.api.categories.Category;
 import fr.poslovitch.slimefunx.api.interfaces.Hashable;
+import net.minecraft.server.v1_13_R2.NBTTagCompound;
 import org.apache.commons.lang.Validate;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_13_R2.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
 public class SlimefunObject implements Hashable {
+
+    private static final String NBT_SLIMEFUN_HASH = "slimefun:hash";
 
     private final String id;
     private final Category category;
@@ -22,6 +26,14 @@ public class SlimefunObject implements Hashable {
 
         this.id = id;
         this.category = category;
+
+        // Add the NBTTag to the item
+        net.minecraft.server.v1_13_R2.ItemStack nmsStack = CraftItemStack.asNMSCopy(item);
+        NBTTagCompound compound = (nmsStack.getTag() != null) ? nmsStack.getTag() : new NBTTagCompound();
+        compound.setString(NBT_SLIMEFUN_HASH, getHash());
+        nmsStack.setTag(compound);
+        item = CraftItemStack.asBukkitCopy(nmsStack);
+
         this.item = item;
     }
 
