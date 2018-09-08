85f3b33d2d080e66540e7001824bdf1aa7d2ffad
Florian CUNY <poslovitch@gmail.com>
2018-09-08 14:21:00 +0200

    Added Validation in SlimefunObject

--- src/main/java/fr/poslovitch/slimefunx/api/base/SlimefunObject.java
+++ src/main/java/fr/poslovitch/slimefunx/api/base/SlimefunObject.java
@@ -2,15 +2,24 @@ package fr.poslovitch.slimefunx.api.base;
 
 import fr.poslovitch.slimefunx.api.categories.Category;
 import fr.poslovitch.slimefunx.api.interfaces.Hashable;
+import org.apache.commons.lang.Validate;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class SlimefunObject implements Hashable {
 
-    private String id;
-    private Category category;
-    private ItemStack item;
+    private final String id;
+    private final Category category;
+    private final ItemStack item;
 
     public SlimefunObject(String id, Category category, ItemStack item) {
+        // Validation - make sure that none of the specified value is illegal.
+        Validate.notNull(id, "Id cannot be null.");
+        Validate.notNull(category, "Category cannot be null.");
+        Validate.notNull(item, "Item cannot be null");
+        Validate.isTrue(!(item.getType().equals(Material.AIR) || item.getType().equals(Material.CAVE_AIR) || item.getType().equals(Material.VOID_AIR)), "Item cannot be of type AIR: ", item.getType());
+        // -------------------------------------------------------------------
+
         this.id = id;
         this.category = category;
         this.item = item;
