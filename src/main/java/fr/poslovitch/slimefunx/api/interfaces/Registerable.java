be646d8cb24f627cabf367d61d9b0d1738fcb69e
Florian CUNY <poslovitch@gmail.com>
2018-09-08 16:11:31 +0200

    Added Registerable interface and made SlimefunItem Registerable
    Signed-off-by: Florian CUNY <poslovitch@gmail.com>

---
+++ src/main/java/fr/poslovitch/slimefunx/api/interfaces/Registerable.java
@@ -0,0 +1,6 @@
+package fr.poslovitch.slimefunx.api.interfaces;
+
+public interface Registerable {
+
+    void register();
+}
--- src/main/java/fr/poslovitch/slimefunx/api/items/SlimefunItem.java
+++ src/main/java/fr/poslovitch/slimefunx/api/items/SlimefunItem.java
@@ -3,11 +3,12 @@ package fr.poslovitch.slimefunx.api.items;
 import fr.poslovitch.slimefunx.api.base.SlimefunObject;
 import fr.poslovitch.slimefunx.api.categories.Category;
 import fr.poslovitch.slimefunx.api.interfaces.Craftable;
+import fr.poslovitch.slimefunx.api.interfaces.Registerable;
 import fr.poslovitch.slimefunx.api.interfaces.Researchable;
 import org.apache.commons.lang.Validate;
 import org.bukkit.inventory.ItemStack;
 
-public class SlimefunItem extends SlimefunObject implements Craftable, Researchable {
+public class SlimefunItem extends SlimefunObject implements Craftable, Researchable, Registerable {
 
     private final String[][] recipePattern;
     private final int researchCost;
@@ -34,5 +35,10 @@ public class SlimefunItem extends SlimefunObject implements Craftable, Researcha
     @Override
     public int getResearchCost() {
         return researchCost;
+    }
+
+    @Override
+    public void register() {
+
     }
 }
