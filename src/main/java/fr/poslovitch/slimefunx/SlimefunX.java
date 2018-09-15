847978ad500cc2c351c4da5de45aece1bd648a15
Florian CUNY <poslovitch@gmail.com>
2018-09-15 22:54:24 +0200

    ItemsManager is now successfully loading items

--- src/main/java/fr/poslovitch/slimefunx/SlimefunX.java
+++ src/main/java/fr/poslovitch/slimefunx/SlimefunX.java
@@ -1,13 +1,18 @@
 package fr.poslovitch.slimefunx;
 
+import fr.poslovitch.slimefunx.managers.ItemsManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SlimefunX extends JavaPlugin {
     private static SlimefunX instance;
 
+    private ItemsManager itemsManager;
+
     @Override
     public void onEnable() {
         setInstance(this);
+        this.itemsManager = new ItemsManager(this);
+        this.itemsManager.registerItems();
     }
 
     @Override
--- src/main/java/fr/poslovitch/slimefunx/managers/ItemsManager.java
+++ src/main/java/fr/poslovitch/slimefunx/managers/ItemsManager.java
@@ -1,30 +1,59 @@
 package fr.poslovitch.slimefunx.managers;
 
 import fr.poslovitch.slimefunx.SlimefunX;
+import fr.poslovitch.slimefunx.api.interfaces.Interactable;
 import fr.poslovitch.slimefunx.api.items.SlimefunItem;
+import fr.poslovitch.slimefunx.lists.SlimefunItems;
 import org.apache.commons.lang.Validate;
+import org.bukkit.Bukkit;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class ItemsManager {
 
     private final SlimefunX plugin;
-    private final List<SlimefunItem> items = new LinkedList<>();
+    private final Map<String, SlimefunItem> items = new LinkedHashMap<>();
 
     public ItemsManager(SlimefunX plugin) {
         this.plugin = plugin;
     }
 
-    public void registerItems() {
-
+    public Map<String, SlimefunItem> getItems() {
+        return items;
     }
 
-    public void registerItem(SlimefunItem item) {
+    public Set<SlimefunItem> getItemsSet() {
+        return new HashSet<>(items.values());
+    }
+
+    public Set<String> getIdsSet() {
+        return items.keySet();
+    }
+
+    public SlimefunItem getItemById(String id) {
+        return items.get(id);
+    }
+
+    public void registerItems() {
+        Arrays.stream(SlimefunItems.class.getDeclaredFields())
+                .filter(field -> SlimefunItem.class.isAssignableFrom(field.getType()))
+                .forEach(field -> {
+                    try {
+                        registerItem((SlimefunItem) field.get(null));
+                    } catch (Exception e) {
+                        plugin.getLogger().info("Skipping '" + field.getName() + "' due to IllegalAccessException.");
+                    }
+                });
+    }
+
+    private void registerItem(SlimefunItem item) {
         Validate.notNull(item, "The item cannot be null.");
 
         try {
-
+            items.put(item.getId(), item);
+            if (item instanceof Interactable) {
+                Bukkit.getPluginManager().registerEvents(((Interactable) item).getInteractionListener(), plugin);
+            }
         } catch (Exception e) {
             plugin.getLogger().severe("Item registration failed: " + item.getId());
         }
