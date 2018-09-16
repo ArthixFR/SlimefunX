9b366d3a5bd05ab77e2bdcc5cdcdcbe6c8dc66f3
Florian CUNY <poslovitch@gmail.com>
2018-09-16 17:13:05 +0200

    Made SlimefunMachine implement CraftingAgent

--- src/main/java/fr/poslovitch/slimefunx/api/machines/SlimefunMachine.java
+++ src/main/java/fr/poslovitch/slimefunx/api/machines/SlimefunMachine.java
@@ -1,13 +1,14 @@
 package fr.poslovitch.slimefunx.api.machines;
 
 import fr.poslovitch.slimefunx.api.categories.Category;
+import fr.poslovitch.slimefunx.api.interfaces.CraftingAgent;
 import fr.poslovitch.slimefunx.api.interfaces.Interactable;
 import fr.poslovitch.slimefunx.api.interfaces.Placeable;
 import fr.poslovitch.slimefunx.api.items.SlimefunItem;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
-public class SlimefunMachine extends SlimefunItem implements Placeable, Interactable {
+public class SlimefunMachine extends SlimefunItem implements Placeable, Interactable, CraftingAgent {
 
     private final Listener interactionListener;
 
@@ -20,5 +21,10 @@ public class SlimefunMachine extends SlimefunItem implements Placeable, Interact
     @Override
     public Listener getInteractionListener() {
         return interactionListener;
+    }
+
+    @Override
+    public void processRecipe(String[][] recipe) {
+        // Do nothing for now
     }
 }
