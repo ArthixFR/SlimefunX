a84af95919215f8fd538bdeb7ae422de70a85be0
Florian CUNY <poslovitch@gmail.com>
2018-09-16 16:11:39 +0200

    Started creating the slimefun guide panel

---
+++ src/main/java/fr/poslovitch/slimefunx/panels/SlimefunGuide.java
@@ -0,0 +1,17 @@
+package fr.poslovitch.slimefunx.panels;
+
+import org.bukkit.entity.Player;
+import world.bentobox.bentobox.api.panels.builders.PanelBuilder;
+
+public class SlimefunGuide {
+
+    private SlimefunGuide(){}
+
+    public static void openMainMenu(Player p) {
+        PanelBuilder panelBuilder = new PanelBuilder();
+    }
+
+    public static void setupHeader() {
+
+    }
+}
