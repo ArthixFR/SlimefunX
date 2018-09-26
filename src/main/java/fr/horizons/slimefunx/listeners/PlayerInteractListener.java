33e2c5eaba13cb852acf3f0b8a79b4c28ded6a71
Julien <julien.thomas018@live.fr>
2018-09-26 16:48:20 +0200

    Fix some interactions with a slimefunObject and added check if an entity present before placing a SlimefunBlock

--- src/main/java/fr/horizons/slimefunx/listeners/PlayerInteractListener.java
+++ src/main/java/fr/horizons/slimefunx/listeners/PlayerInteractListener.java
@@ -1,10 +1,10 @@
 package fr.horizons.slimefunx.listeners;
 
 import fr.horizons.slimefunx.SlimefunX;
+import fr.horizons.slimefunx.base.SlimefunObject;
 import fr.horizons.slimefunx.block.SlimefunBlock;
 import fr.horizons.slimefunx.block.SlimefunMachine;
 import fr.horizons.slimefunx.block.SlimefunStaticBlock;
-import fr.horizons.slimefunx.item.SlimefunItem;
 import fr.horizons.slimefunx.item.SlimefunTool;
 import fr.horizons.slimefunx.util.LocationUtils;
 import org.bukkit.Location;
@@ -13,6 +13,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
 public class PlayerInteractListener implements Listener {
     private SlimefunX plugin;
@@ -21,22 +22,29 @@ public class PlayerInteractListener implements Listener {
         this.plugin = plugin;
     }
 
-    // TODO: CHECK SI UNE ENTITÉ A LA POSITION !
-
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
-        SlimefunItem slimefunItem = plugin.getItemsManager().getItemByTag(e.getItem());
-        SlimefunBlock slimefunBlock = plugin.getBlocksManager().getBlockByTag(e.getItem());
-        if (slimefunItem instanceof SlimefunTool) {
-            if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && (e.getClickedBlock().getType().equals(Material.DIRT) || e.getClickedBlock().getType().equals(Material.COARSE_DIRT) || e.getClickedBlock().getType().equals(Material.GRASS_BLOCK) || e.getClickedBlock().getType().equals(Material.GRASS_PATH))) e.setCancelled(true);
-            ((SlimefunTool) slimefunItem).interactEvent(e);
-        }
+        if (e.getClickedBlock() == null) return;
+
+        SlimefunObject slimefunObjectItem = plugin.getItemsManager().getItemByTag(e.getItem()) == null ? plugin.getBlocksManager().getBlockByTag(e.getItem()) : plugin.getItemsManager().getItemByTag(e.getItem());
+        SlimefunBlock slimefunBlock = plugin.getBlocksManager().getBlockByTag(e.getClickedBlock());
+
         if (slimefunBlock instanceof SlimefunStaticBlock || slimefunBlock instanceof SlimefunMachine) {
-            slimefunBlock.interactEvent(e);
+            if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && e.getHand().equals(EquipmentSlot.HAND) && !e.getPlayer().isSneaking()) {
+                e.setCancelled(true);
+                slimefunBlock.interactEvent(e);
+                return;
+            }
+        }
+
+        if (slimefunObjectItem instanceof SlimefunTool) {
+            if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && (e.getClickedBlock().getType().equals(Material.DIRT) || e.getClickedBlock().getType().equals(Material.COARSE_DIRT) || e.getClickedBlock().getType().equals(Material.GRASS_BLOCK) || e.getClickedBlock().getType().equals(Material.GRASS_PATH))) e.setCancelled(true);
+            ((SlimefunTool) slimefunObjectItem).interactEvent(e);
+        } else if (slimefunObjectItem instanceof SlimefunStaticBlock || slimefunObjectItem instanceof SlimefunMachine) {
             if ((e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && !e.getClickedBlock().getType().isInteractable()) || (e.getAction().equals(Action.RIGHT_CLICK_BLOCK) && e.getClickedBlock().getType().isInteractable() && e.getPlayer().isSneaking())) {
                 e.setCancelled(true);
                 Location newLoc = LocationUtils.getPlaceLocation(e.getClickedBlock().getLocation(), e.getBlockFace());
-                if (LocationUtils.isPlaceEmpty(newLoc)) slimefunBlock.placeBlock(e.getItem(), newLoc);
+                if (LocationUtils.isPlaceAir(newLoc) && LocationUtils.isPlaceEmpty(newLoc)) ((SlimefunBlock) slimefunObjectItem).placeBlock(e.getItem(), newLoc);
             }
         }
     }
