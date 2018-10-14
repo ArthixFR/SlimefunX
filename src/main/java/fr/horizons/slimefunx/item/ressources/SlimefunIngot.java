39290299b408ac341cf0fb6e542bc374c31d1549
Julien <julien.thomas018@live.fr>
2018-10-14 08:33:53 +0200

    Optimized nugget and ingot recipes patterns

--- src/main/java/fr/horizons/slimefunx/item/ressources/SlimefunIngot.java
+++ src/main/java/fr/horizons/slimefunx/item/ressources/SlimefunIngot.java
@@ -2,13 +2,15 @@ package fr.horizons.slimefunx.item.ressources;
 
 import com.google.common.collect.ImmutableTable;
 import com.google.common.collect.Table;
+import fr.horizons.slimefunx.SlimefunX;
+import fr.horizons.slimefunx.item.SlimefunItem;
 import fr.horizons.slimefunx.item.SlimefunResource;
-import fr.horizons.slimefunx.list.ItemList;
 import fr.horizons.slimefunx.util.CraftingType;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.logging.Level;
 
 public class SlimefunIngot extends SlimefunResource {
 
@@ -18,71 +20,19 @@ public class SlimefunIngot extends SlimefunResource {
 
     @Override
     public Table<CraftingType, Integer, ItemStack[]> getRecipesPatterns() {
-        if (getId().equals(ItemList.ALUMINIUM_INGOT.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.ALUMINIUM_NUGGET.getItem(), ItemList.ALUMINIUM_NUGGET.getItem(), ItemList.ALUMINIUM_NUGGET.getItem(),
-                                    ItemList.ALUMINIUM_NUGGET.getItem(), ItemList.ALUMINIUM_NUGGET.getItem(), ItemList.ALUMINIUM_NUGGET.getItem(),
-                                    ItemList.ALUMINIUM_NUGGET.getItem(), ItemList.ALUMINIUM_NUGGET.getItem(), ItemList.ALUMINIUM_NUGGET.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.COPPER_INGOT.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.COPPER_NUGGET.getItem(), ItemList.COPPER_NUGGET.getItem(), ItemList.COPPER_NUGGET.getItem(),
-                                    ItemList.COPPER_NUGGET.getItem(), ItemList.COPPER_NUGGET.getItem(), ItemList.COPPER_NUGGET.getItem(),
-                                    ItemList.COPPER_NUGGET.getItem(), ItemList.COPPER_NUGGET.getItem(), ItemList.COPPER_NUGGET.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.LEAD_INGOT.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.LEAD_NUGGET.getItem(), ItemList.LEAD_NUGGET.getItem(), ItemList.LEAD_NUGGET.getItem(),
-                                    ItemList.LEAD_NUGGET.getItem(), ItemList.LEAD_NUGGET.getItem(), ItemList.LEAD_NUGGET.getItem(),
-                                    ItemList.LEAD_NUGGET.getItem(), ItemList.LEAD_NUGGET.getItem(), ItemList.LEAD_NUGGET.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.MAGNESIUM_INGOT.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.MAGNESIUM_NUGGET.getItem(), ItemList.MAGNESIUM_NUGGET.getItem(), ItemList.MAGNESIUM_NUGGET.getItem(),
-                                    ItemList.MAGNESIUM_NUGGET.getItem(), ItemList.MAGNESIUM_NUGGET.getItem(), ItemList.MAGNESIUM_NUGGET.getItem(),
-                                    ItemList.MAGNESIUM_NUGGET.getItem(), ItemList.MAGNESIUM_NUGGET.getItem(), ItemList.MAGNESIUM_NUGGET.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.SILVER_INGOT.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.SILVER_NUGGET.getItem(), ItemList.SILVER_NUGGET.getItem(), ItemList.SILVER_NUGGET.getItem(),
-                                    ItemList.SILVER_NUGGET.getItem(), ItemList.SILVER_NUGGET.getItem(), ItemList.SILVER_NUGGET.getItem(),
-                                    ItemList.SILVER_NUGGET.getItem(), ItemList.SILVER_NUGGET.getItem(), ItemList.SILVER_NUGGET.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.TIN_INGOT.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.TIN_NUGGET.getItem(), ItemList.TIN_NUGGET.getItem(), ItemList.TIN_NUGGET.getItem(),
-                                    ItemList.TIN_NUGGET.getItem(), ItemList.TIN_NUGGET.getItem(), ItemList.TIN_NUGGET.getItem(),
-                                    ItemList.TIN_NUGGET.getItem(), ItemList.TIN_NUGGET.getItem(), ItemList.TIN_NUGGET.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.ZINC_INGOT.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.ZINC_NUGGET.getItem(), ItemList.ZINC_NUGGET.getItem(), ItemList.ZINC_NUGGET.getItem(),
-                                    ItemList.ZINC_NUGGET.getItem(), ItemList.ZINC_NUGGET.getItem(), ItemList.ZINC_NUGGET.getItem(),
-                                    ItemList.ZINC_NUGGET.getItem(), ItemList.ZINC_NUGGET.getItem(), ItemList.ZINC_NUGGET.getItem()
-                            })
-                    .build();
+        String dustId = getId().split("_")[0];
+        SlimefunItem slimefunItem = SlimefunX.getInstance().getItemsManager().getItemById(dustId + "_nugget");
+        if (slimefunItem == null) {
+            SlimefunX.getInstance().getLogger().log(Level.SEVERE, "Failed to register craft for " + getId() + " !");
+            return null;
+        } else {
+            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder().put(CraftingType.CRAFTING_TABLE, 1,
+                    new ItemStack[]{
+                            slimefunItem.getItem(),slimefunItem.getItem(),slimefunItem.getItem(),
+                            slimefunItem.getItem(),slimefunItem.getItem(),slimefunItem.getItem(),
+                            slimefunItem.getItem(),slimefunItem.getItem(),slimefunItem.getItem(),
+                    }).build();
         }
-        return null;
     }
 
     @Override
--- src/main/java/fr/horizons/slimefunx/item/ressources/SlimefunNugget.java
+++ src/main/java/fr/horizons/slimefunx/item/ressources/SlimefunNugget.java
@@ -2,14 +2,15 @@ package fr.horizons.slimefunx.item.ressources;
 
 import com.google.common.collect.ImmutableTable;
 import com.google.common.collect.Table;
+import fr.horizons.slimefunx.SlimefunX;
+import fr.horizons.slimefunx.item.SlimefunItem;
 import fr.horizons.slimefunx.item.SlimefunResource;
-import fr.horizons.slimefunx.list.ItemList;
 import fr.horizons.slimefunx.util.CraftingType;
-import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.logging.Level;
 
 public class SlimefunNugget extends SlimefunResource {
 
@@ -19,71 +20,19 @@ public class SlimefunNugget extends SlimefunResource {
 
     @Override
     public Table<CraftingType, Integer, ItemStack[]> getRecipesPatterns() {
-        if (getId().equals(ItemList.ALUMINIUM_NUGGET.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.ALUMINIUM_DUST.getItem(), ItemList.ALUMINIUM_DUST.getItem(), ItemList.ALUMINIUM_DUST.getItem(),
-                                    ItemList.ALUMINIUM_DUST.getItem(), ItemList.ALUMINIUM_DUST.getItem(), ItemList.ALUMINIUM_DUST.getItem(),
-                                    ItemList.ALUMINIUM_DUST.getItem(), ItemList.ALUMINIUM_DUST.getItem(), ItemList.ALUMINIUM_DUST.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.COPPER_NUGGET.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.COPPER_DUST.getItem(), ItemList.COPPER_DUST.getItem(), ItemList.COPPER_DUST.getItem(),
-                                    ItemList.COPPER_DUST.getItem(), ItemList.COPPER_DUST.getItem(), ItemList.COPPER_DUST.getItem(),
-                                    ItemList.COPPER_DUST.getItem(), ItemList.COPPER_DUST.getItem(), ItemList.COPPER_DUST.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.LEAD_NUGGET.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.LEAD_DUST.getItem(), ItemList.LEAD_DUST.getItem(), ItemList.LEAD_DUST.getItem(),
-                                    ItemList.LEAD_DUST.getItem(), ItemList.LEAD_DUST.getItem(), ItemList.LEAD_DUST.getItem(),
-                                    ItemList.LEAD_DUST.getItem(), ItemList.LEAD_DUST.getItem(), ItemList.LEAD_DUST.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.MAGNESIUM_NUGGET.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.MAGNESIUM_DUST.getItem(), ItemList.MAGNESIUM_DUST.getItem(), ItemList.MAGNESIUM_DUST.getItem(),
-                                    ItemList.MAGNESIUM_DUST.getItem(), ItemList.MAGNESIUM_DUST.getItem(), ItemList.MAGNESIUM_DUST.getItem(),
-                                    ItemList.MAGNESIUM_DUST.getItem(), ItemList.MAGNESIUM_DUST.getItem(), ItemList.MAGNESIUM_DUST.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.SILVER_NUGGET.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.SILVER_DUST.getItem(), ItemList.SILVER_DUST.getItem(), ItemList.SILVER_DUST.getItem(),
-                                    ItemList.SILVER_DUST.getItem(), ItemList.SILVER_DUST.getItem(), ItemList.SILVER_DUST.getItem(),
-                                    ItemList.SILVER_DUST.getItem(), ItemList.SILVER_DUST.getItem(), ItemList.SILVER_DUST.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.TIN_NUGGET.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.TIN_DUST.getItem(), ItemList.TIN_DUST.getItem(), ItemList.TIN_DUST.getItem(),
-                                    ItemList.TIN_DUST.getItem(), ItemList.TIN_DUST.getItem(), ItemList.TIN_DUST.getItem(),
-                                    ItemList.TIN_DUST.getItem(), ItemList.TIN_DUST.getItem(), ItemList.TIN_DUST.getItem()
-                            })
-                    .build();
-        } else if (getId().equals(ItemList.ZINC_NUGGET.getId())) {
-            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder()
-                    .put(CraftingType.CRAFTING_TABLE, 1,
-                            new ItemStack[]{
-                                    ItemList.ZINC_DUST.getItem(), ItemList.ZINC_DUST.getItem(), ItemList.ZINC_DUST.getItem(),
-                                    ItemList.ZINC_DUST.getItem(), ItemList.ZINC_DUST.getItem(), ItemList.ZINC_DUST.getItem(),
-                                    ItemList.ZINC_DUST.getItem(), ItemList.ZINC_DUST.getItem(), ItemList.ZINC_DUST.getItem()
-                            })
-                    .build();
+        String nuggetId = getId().split("_")[0];
+        SlimefunItem slimefunItem = SlimefunX.getInstance().getItemsManager().getItemById(nuggetId + "_dust");
+        if (slimefunItem == null) {
+            SlimefunX.getInstance().getLogger().log(Level.SEVERE, "Failed to register craft for " + getId() + " !");
+            return null;
+        } else {
+            return ImmutableTable.<CraftingType, Integer, ItemStack[]>builder().put(CraftingType.CRAFTING_TABLE, 1,
+                    new ItemStack[]{
+                            slimefunItem.getItem(),slimefunItem.getItem(),slimefunItem.getItem(),
+                            slimefunItem.getItem(),slimefunItem.getItem(),slimefunItem.getItem(),
+                            slimefunItem.getItem(),slimefunItem.getItem(),slimefunItem.getItem(),
+                    }).build();
         }
-        return null;
     }
 
     @Override
