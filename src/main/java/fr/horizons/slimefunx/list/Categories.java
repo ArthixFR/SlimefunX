b2a037d3d8b6b0391d98bb40bbd58d235d212635
Julien <julien.thomas018@live.fr>
2018-10-17 20:57:33 +0200

    Added name and lore for categories. Added a method to search categories by id

--- src/main/java/fr/horizons/slimefunx/list/Categories.java
+++ src/main/java/fr/horizons/slimefunx/list/Categories.java
@@ -4,9 +4,26 @@ import fr.horizons.slimefunx.base.Category;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Arrays;
+
 public class Categories {
-    public static final Category TOOLS = new Category("TOOLS", new ItemStack(Material.WOODEN_AXE));
-    public static final Category STATIC_BLOCKS = new Category("STATIC_BLOCKS", new ItemStack(Material.WOODEN_AXE));
-    public static final Category MACHINES = new Category("MACHINES", new ItemStack(Material.IRON_BLOCK));
-    public static final Category RESSOURCES = new Category("RESSOURCES", new ItemStack(Material.REDSTONE));
+    public static final Category TOOLS = new Category("TOOLS", new ItemStack(Material.WOODEN_AXE), "Outils", null);
+    public static final Category STATIC_BLOCKS = new Category("STATIC_BLOCKS", new ItemStack(Material.WOODEN_AXE), "Blocs", null);
+    public static final Category MACHINES = new Category("MACHINES", new ItemStack(Material.IRON_BLOCK), "Machines", null);
+    public static final Category RESSOURCES = new Category("RESSOURCES", new ItemStack(Material.REDSTONE), "Ressources", null);
+
+    public static Category getCategoryById(String id) {
+        final Category[] category_ = new Category[1];
+        Arrays.stream(Categories.class.getDeclaredFields())
+                .filter(field -> Category.class.isAssignableFrom(field.getType()))
+                .forEach(field -> {
+                    try {
+                        Category category = (Category) field.get(null);
+                        if (category.getId().equalsIgnoreCase(id)) category_[0] = category;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+        return category_[0];
+    }
 }
