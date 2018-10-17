e0067e29c8fd65bb73329bc0792a8036511a93ec
Julien <julien.thomas018@live.fr>
2018-10-17 21:00:00 +0200

    When creating a category, the item has now a proper name, lore and all attributes are hidden

--- src/main/java/fr/horizons/slimefunx/base/Category.java
+++ src/main/java/fr/horizons/slimefunx/base/Category.java
@@ -1,16 +1,38 @@
 package fr.horizons.slimefunx.base;
 
 import fr.horizons.slimefunx.interfaces.IIdentifiable;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
 
 public class Category implements IIdentifiable {
 
     private final String id;
-    private final ItemStack icon;
+    private String name;
+    private List<String> lore;
+    private ItemStack icon;
 
-    public Category(String id, ItemStack icon) {
+    public Category(String id, ItemStack icon, String name, List<String> lore) {
         this.id = id;
         this.icon = icon;
+        this.name = name;
+        this.lore = lore;
+
+        ItemMeta im = this.getIcon().getItemMeta();
+        im.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_POTION_EFFECTS, ItemFlag.HIDE_ENCHANTS);
+
+        if (name != null) {
+            this.name = "§f" + name;
+            im.setDisplayName(this.name);
+        }
+        if (lore != null) {
+            this.lore = lore;
+            im.setLore(lore);
+        }
+        this.getIcon().setItemMeta(im);
+        this.setIcon(this.getIcon());
     }
 
     @Override
@@ -20,5 +42,17 @@ public class Category implements IIdentifiable {
 
     public ItemStack getIcon() {
         return icon;
+    }
+
+    public List<String> getLore() {
+        return lore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setIcon(ItemStack icon) {
+        this.icon = icon;
     }
 }
