3043856b51720a558bc4303d6ac9f063aa874311
Julien <julien.thomas018@live.fr>
2018-10-17 21:03:20 +0200

    Added name and lore for SlimefunGuiResource, and remove all attributes

--- src/main/java/fr/horizons/slimefunx/gui/SlimefunGuiResource.java
+++ src/main/java/fr/horizons/slimefunx/gui/SlimefunGuiResource.java
@@ -3,16 +3,32 @@ package fr.horizons.slimefunx.gui;
 import net.minecraft.server.v1_13_R2.NBTTagCompound;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_13_R2.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 import static fr.horizons.slimefunx.base.SlimefunObject.NBT_SLIMEFUN_ID;
 
 public abstract class SlimefunGuiResource extends SlimefunGui {
     private int textureId;
+    private String name;
+    private List<String> lore;
+
+    public SlimefunGuiResource(String id, int textureId, @Nullable String name, @Nullable List<String> lore) {
+        super(id);
+        this.textureId = textureId;
+        this.name = name;
+        this.lore = lore;
+    }
 
     public SlimefunGuiResource(String id, int textureId) {
         super(id);
         this.textureId = textureId;
+        this.name = null;
+        this.lore = null;
     }
 
     public boolean hasCustomTexture() {
@@ -22,11 +38,16 @@ public abstract class SlimefunGuiResource extends SlimefunGui {
     public ItemStack getCustomTexture() {
         if (!hasCustomTexture()) return new ItemStack(Material.AIR);
         ItemStack is = new ItemStack(Material.DIAMOND_SWORD);
+        ItemMeta im = is.getItemMeta();
+        im.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE, ItemFlag.HIDE_ENCHANTS);
+        im.setDisplayName(name == null ? "§0" : "§f" + name);
+        if (lore != null) im.setLore(lore);
+        is.setItemMeta(im);
+
         net.minecraft.server.v1_13_R2.ItemStack nmsStack = CraftItemStack.asNMSCopy(is);
         NBTTagCompound compound = (nmsStack.hasTag()) ? nmsStack.getTag() : new NBTTagCompound();
         compound.setString(NBT_SLIMEFUN_ID, getId());
         // Bind to custom texture, if textureId equals -1, custom texture is not loaded
-        System.out.println(textureId);
         if (this.textureId != -1) {
             compound.setInt("Damage", textureId);
             compound.setBoolean("Unbreakable", true);
